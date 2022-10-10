46255746abbc3cf938e73580e358195dd39977de
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2022-10-10 19:58:50 +0800

    pm2 metrics support

--- src/metrics/main.js
+++ src/metrics/main.js
@@ -4,17 +4,29 @@ const ios = {
     counter: {
         requestCounter: io.counter({
             name: 'Total requests',
-            id: 'app/total/requests',
+            id: 'app/requests/total'
         }),
 
         pcl2RequestCounter: io.counter({
             name: 'Total requests',
-            id: 'app/total/requests',
+            id: 'app/requests/pcl2'
         }),
+        whitelistRequestCounter: io.counter({
+            name: 'Whitelist requests',
+            id: 'app/requests/whitelist'
+        }),
+        userRequestCounter: io.counter({
+            name: 'User requests',
+            id: 'app/requests/user'
+        }),
+        serverRequestCounter: io.counter({
+            name: 'MC server requests',
+            id: 'app/requests/server'
+        })
     },
 };
 
-const RequestTypes = {
+export let RequestTypes = {
     USER: 0,
     PCL2: 1,
     WHITELIST: 2,
@@ -26,8 +38,20 @@ const RequestTypes = {
 export let addRequestTotal = (type = RequestTypes.UNKNOWN) => {
     ios.counter.requestCounter.inc();
     switch (type) {
+        case RequestTypes.USER:
+            ios.counter.userRequestCounter.inc();
+            break;
+
         case RequestTypes.PCL2:
             ios.counter.pcl2RequestCounter.inc();
+            break;
+
+        case RequestTypes.WHITELIST:
+            ios.counter.whitelistRequestCounter.inc();
+            break;
+
+        case RequestTypes.SERVER:
+            ios.counter.serverRequestCounter.inc();
             break;
 
         default:
